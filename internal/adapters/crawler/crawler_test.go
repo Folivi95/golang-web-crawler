e19da9e20f2bc38da39e85e9defb97b59d341695
Joshua Folivi <joshua.folivi@gmail.com>
2023-02-08 19:31:43 +0000

    implement test for links extractor

--- internal/adapters/crawler/crawler_test.go
+++ internal/adapters/crawler/crawler_test.go
@@ -1,1 +1,20 @@
 package crawler
+
+import "testing"
+
+func TestToFixedUrl(t *testing.T) {
+	fixedUrl := toFixedUrl("http://ajinkya.com/aboutus.html", "http://ajinkya.com/")
+	if fixedUrl != "http://ajinkya.com/aboutus.html" {
+		t.Error("toFixedUrl did not get expected href")
+	}
+
+	mailToUrl := toFixedUrl("mailto:ajinkya@gmail.com", "http://ajinkya.com/")
+	if mailToUrl != "http://ajinkya.com/" {
+		t.Error("expected baseUrl instead of mailto link")
+	}
+
+	telephoneUrl := toFixedUrl("tel://9820098200", "http://ajinkya.com/")
+	if telephoneUrl != "http://ajinkya.com/" {
+		t.Error("expected baseUrl instead of telephone link")
+	}
+}
